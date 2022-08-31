0f3364aec8f03ddd8431db20a8697328fa989e51
ryfazrin <riansera515@gmail.com>
2022-08-31 14:03:23 +0700

    standard: create standard thirty party

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,8 +19,17 @@ const config: PlaywrightTestConfig = {
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
+  testMatch: [
+    'tests/integrations/adobe-launch/adobe-launch.spec.ts',
+    'tests/integrations/facebook-pixel/facebook-pixel.spec.ts',
+    'tests/integrations/gtm/gtm.spec.ts',
+    'tests/integrations/jquery/jquery.spec.ts',
+    'tests/integrations/main-window-accessors/main-window-accessors.spec.ts',
+    'tests/integrations/mermaid/mermaid.spec.ts',
+    'tests/integrations/twitter/twitter.spec.ts'
+  ]
   // timeout: 60000,
-  testMatch: 'tests/integrations/**/**.standard.spec.ts',
+  // testMatch: 'tests/integrations/**/**.standard.spec.ts',
   // testMatch: 'tests/integrations/gtm/gtm.spec.ts' // error in firefox
   // testMatch: 'tests/platform/event/event.spec.ts' // error in webkit
 };
